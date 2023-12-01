bb1cc8161617aa6c02ebe828ea70f8232f8516a0
vinayraj010 <vinayraj034@gmail.com>
2023-12-01 18:24:56 +0530

    third

--- flutter_bloc_cart/lib/main.dart
+++ flutter_bloc_cart/lib/main.dart
@@ -4,11 +4,9 @@ import 'package:flutter_bloc_cart/features/home/ui/home.dart';
 void main(){
   runApp( const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
